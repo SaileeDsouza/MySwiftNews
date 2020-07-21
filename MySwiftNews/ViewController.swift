27db930d789b2876187f63874d8467f1296108b1
Sailee Pereira <sailee.pereira@clearbridgemobile.com>
2020-07-21 14:43:19 -0400

    Initial Commit

---
+++ MySwiftNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySwiftNews
+//
+//  Created by Sailee Pereira on 2020-07-21.
+//  Copyright © 2020 SaileePereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
